15e5a3472c0e0b1f409809ed8c1df0b5543ca39f
romeluis <113552563+romeluis@users.noreply.github.com>
2023-02-17 12:31:43 -0500

    added dark mode to index

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,50 @@
+
 //Variables to keep track if videos have already been played
 var projectPlayed = 0;
 var aboutPlayed = 0;
 
 //Main function block to run after webpage has fully loaded
 $(document).ready(function(){
-
     // Lists pointing to changes & random number generator
-    var cssList = ["css/home/home-stylesheet-green.css", "css/home/home-stylesheet-red.css", "css/home/home-stylesheet-yellow.css", "css/home/home-stylesheet-pink.css"];
-    var faviconList = ["images/favicons/logo_green.ico", "images/favicons/logo_red.ico", "images/favicons/logo_yellow.ico", "images/favicons/logo_pink.ico"];
-    var projectsVideoList = ["videos/home-projects-red.mp4", "videos/home-projects-green.mp4", "videos/home-projects-green.mp4", "videos/home-projects-green.mp4"];
+    var cssList = ["css/home/home-stylesheet-green.css", "css/home/home-stylesheet-red.css", "css/home/home-stylesheet-yellow.css", "css/home/home-stylesheet-pink.css","css/home/home-stylesheet-green-dark.css", "css/home/home-stylesheet-red-dark.css", "css/home/home-stylesheet-yellow-dark.css", "css/home/home-stylesheet-pink-dark.css"];
+    var faviconList = ["images/favicons/logo_green.ico", "images/favicons/logo_red.ico", "images/favicons/logo_yellow.ico", "images/favicons/logo_pink.ico", "images/favicons/logo_green_dark.ico", "images/favicons/logo_red_dark.ico", "images/favicons/logo_yellow_dark.ico", "images/favicons/logo_pink_dark.ico"];
+    var projectsVideoList = ["videos/home-projects-red.mp4", "videos/home-projects-green.mp4", "videos/home-projects-green.mp4", "videos/home-projects-green.mp4", "videos/home-projects-red-dark.mp4", "videos/home-projects-green-dark.mp4", "videos/home-projects-green-dark.mp4", "videos/home-projects-green-dark.mp4"];
     var aboutVideoList = ["videos/home-about-pink.mp4", "videos/home-about-pink.mp4", "videos/home-about-red.mp4", "videos/home-about-red.mp4"];
     var projectsArrow = ["filter: invert(14%) sepia(75%) saturate(7467%) hue-rotate(346deg) brightness(94%) contrast(116%);", "filter: invert(70%) sepia(10%) saturate(2906%) hue-rotate(44deg) brightness(97%) contrast(83%);", "filter: invert(70%) sepia(10%) saturate(2906%) hue-rotate(44deg) brightness(97%) contrast(83%);", "filter: invert(70%) sepia(10%) saturate(2906%) hue-rotate(44deg) brightness(97%) contrast(83%);"];
     var aboutArrow = ["filter: invert(80%) sepia(16%) saturate(763%) hue-rotate(302deg) brightness(101%) contrast(105%);", "filter: invert(80%) sepia(16%) saturate(763%) hue-rotate(302deg) brightness(101%) contrast(105%);", "filter: invert(14%) sepia(75%) saturate(7467%) hue-rotate(346deg) brightness(94%) contrast(116%);", "filter: invert(14%) sepia(75%) saturate(7467%) hue-rotate(346deg) brightness(94%) contrast(116%);"];
     var footerArrow = ["invert(2%) sepia(15%) saturate(3681%) hue-rotate(347deg) brightness(94%) contrast(84%);", "invert(2%) sepia(15%) saturate(3681%) hue-rotate(347deg) brightness(94%) contrast(84%);", "images/white-arrow-right.sinvert(90%) sepia(20%) saturate(27%) hue-rotate(296deg) brightness(104%) contrast(99%);vg", "invert(2%) sepia(15%) saturate(3681%) hue-rotate(347deg) brightness(94%) contrast(84%);"];
     var rand = Math.floor(Math.random()*4);
 
-    // Change Video src
-    $("#css-selector").attr("href", cssList[rand]);
-    document.getElementById("index-favicon").setAttribute("href", faviconList[rand]);
-    document.getElementById("projects-video-source").setAttribute("src", projectsVideoList[rand]);
-    document.getElementById("projects-video").load();
-    document.getElementById("about-video-source").setAttribute("src", aboutVideoList[rand]);
-    document.getElementById("about-video").load();
-    //Change img src
-    document.getElementById("projects-arrow").setAttribute("style", projectsArrow[rand]);
-    document.getElementById("about-arrow").setAttribute("style", aboutArrow[rand]);
-    document.getElementById("footer-arrow").setAttribute("style", footerArrow[rand]);
+    //Dark theme and lottie objects
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+    const heroAnimation = document.getElementById("hero-animation");
+    
+    
+    if (prefersDarkScheme.matches) {
+        $("#css-selector").attr("href", cssList[rand+4]);
+        heroAnimation.load("https://lottie.host/13ca4d99-d8fc-48e8-ae57-deeea01bb7a0/chlRqhOqFk.json");
+        document.getElementById("hero-arrow").setAttribute("src", "images/arrow-down-dark.svg");
+        document.getElementById("index-favicon").setAttribute("href", faviconList[rand+4]);
+        document.getElementById("projects-video-source").setAttribute("src", projectsVideoList[rand+4]);
+        document.getElementById("projects-video").load();
+        document.getElementById("about-video-source").setAttribute("src", aboutVideoList[rand+4]);
+        document.getElementById("about-video").load();
+        document.getElementById("projects-arrow").setAttribute("style", projectsArrow[rand]);
+        document.getElementById("about-arrow").setAttribute("style", aboutArrow[rand]);
+        document.getElementById("footer-arrow").setAttribute("style", footerArrow[rand]);
+    } else {
+        //Set light theme
+        $("#css-selector").attr("href", cssList[rand]);
+        heroAnimation.load("https://lottie.host/13ca4d99-d8fc-48e8-ae57-deeea01bb7a0/chlRqhOqFk.json");
+        document.getElementById("index-favicon").setAttribute("href", faviconList[rand]);
+        document.getElementById("projects-video-source").setAttribute("src", projectsVideoList[rand]);
+        document.getElementById("projects-video").load();
+        document.getElementById("about-video-source").setAttribute("src", aboutVideoList[rand]);
+        document.getElementById("about-video").load();
+        document.getElementById("projects-arrow").setAttribute("style", projectsArrow[rand]);
+        document.getElementById("about-arrow").setAttribute("style", aboutArrow[rand]);
+        document.getElementById("footer-arrow").setAttribute("style", footerArrow[rand]);
+    }
     
     //Check if videos are in frame on first load
     //Project Video
