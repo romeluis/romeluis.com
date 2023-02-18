e425506c6a3cc40b1fdcd5ec5502bff9b3bd8ff6
romeluis <113552563+romeluis@users.noreply.github.com>
2023-02-18 17:29:15 -0500

    converted projects video to lottie animation

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
     // Lists pointing to changes & random number generator
     var cssList = ["css/home/home-stylesheet-green.css", "css/home/home-stylesheet-red.css", "css/home/home-stylesheet-yellow.css", "css/home/home-stylesheet-pink.css","css/home/home-stylesheet-green-dark.css", "css/home/home-stylesheet-red-dark.css", "css/home/home-stylesheet-yellow-dark.css", "css/home/home-stylesheet-pink-dark.css"];
     var faviconList = ["images/favicons/logo_green.ico", "images/favicons/logo_red.ico", "images/favicons/logo_yellow.ico", "images/favicons/logo_pink.ico", "images/favicons/logo_green_dark.ico", "images/favicons/logo_red_dark.ico", "images/favicons/logo_yellow_dark.ico", "images/favicons/logo_pink_dark.ico"];
-    var projectsVideoList = ["videos/home-projects-red.mp4", "videos/home-projects-green.mp4", "videos/home-projects-green.mp4", "videos/home-projects-green.mp4", "videos/home-projects-red-dark.mp4", "videos/home-projects-green-dark.mp4", "videos/home-projects-green-dark.mp4", "videos/home-projects-green-dark.mp4"];
+    var projectsVideoList = ["https://lottie.host/542d74f6-6563-4220-8c81-00a9a42bdf73/fwwj9vGrwp.json", "https://lottie.host/5abc9adb-ba9e-4f8e-b54f-18232343224e/zn7QUcRIRu.json", "https://lottie.host/5abc9adb-ba9e-4f8e-b54f-18232343224e/zn7QUcRIRu.json", "https://lottie.host/5abc9adb-ba9e-4f8e-b54f-18232343224e/zn7QUcRIRu.json"];
     var aboutVideoList = ["videos/home-about-pink.mp4", "videos/home-about-pink.mp4", "videos/home-about-red.mp4", "videos/home-about-red.mp4"];
     var projectsArrow = ["filter: invert(14%) sepia(75%) saturate(7467%) hue-rotate(346deg) brightness(94%) contrast(116%);", "filter: invert(70%) sepia(10%) saturate(2906%) hue-rotate(44deg) brightness(97%) contrast(83%);", "filter: invert(70%) sepia(10%) saturate(2906%) hue-rotate(44deg) brightness(97%) contrast(83%);", "filter: invert(70%) sepia(10%) saturate(2906%) hue-rotate(44deg) brightness(97%) contrast(83%);", "filter: invert(97%) sepia(30%) saturate(154%) hue-rotate(286deg) brightness(100%) contrast(98%);", "filter: invert(97%) sepia(30%) saturate(154%) hue-rotate(286deg) brightness(100%) contrast(98%);", "filter: invert(97%) sepia(30%) saturate(154%) hue-rotate(286deg) brightness(100%) contrast(98%);", "filter: invert(97%) sepia(30%) saturate(154%) hue-rotate(286deg) brightness(100%) contrast(98%);"];
     var aboutArrow = ["filter: invert(80%) sepia(16%) saturate(763%) hue-rotate(302deg) brightness(101%) contrast(105%);", "filter: invert(80%) sepia(16%) saturate(763%) hue-rotate(302deg) brightness(101%) contrast(105%);", "filter: invert(14%) sepia(75%) saturate(7467%) hue-rotate(346deg) brightness(94%) contrast(116%);", "filter: invert(14%) sepia(75%) saturate(7467%) hue-rotate(346deg) brightness(94%) contrast(116%);", "filter: invert(97%) sepia(30%) saturate(154%) hue-rotate(286deg) brightness(100%) contrast(98%);", "filter: invert(97%) sepia(30%) saturate(154%) hue-rotate(286deg) brightness(100%) contrast(98%);", "filter: invert(97%) sepia(30%) saturate(154%) hue-rotate(286deg) brightness(100%) contrast(98%);", "filter: invert(97%) sepia(30%) saturate(154%) hue-rotate(286deg) brightness(100%) contrast(98%);"];
@@ -18,6 +18,7 @@ $(document).ready(function(){
     //Dark theme and lottie objects
     const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
     const heroAnimation = document.getElementById("hero-animation");
+    const projectsAnimation = document.getElementById("projects-animation");
     
     
     if (prefersDarkScheme.matches) {
@@ -25,8 +26,7 @@ $(document).ready(function(){
         heroAnimation.load("https://lottie.host/13ca4d99-d8fc-48e8-ae57-deeea01bb7a0/chlRqhOqFk.json");
         document.getElementById("hero-arrow").setAttribute("src", "images/arrow-down-dark.svg");
         document.getElementById("index-favicon").setAttribute("href", faviconList[rand+4]);
-        document.getElementById("projects-video-source").setAttribute("src", projectsVideoList[rand+4]);
-        document.getElementById("projects-video").load();
+        projectsAnimation.load(projectsVideoList[rand])
         document.getElementById("about-video-source").setAttribute("src", aboutVideoList[rand+4]);
         document.getElementById("about-video").load();
         document.getElementById("projects-arrow").setAttribute("style", projectsArrow[rand+4]);
@@ -46,16 +46,17 @@ $(document).ready(function(){
         document.getElementById("footer-arrow").setAttribute("style", footerArrow[rand]);
     }
     
-    //Check if videos are in frame on first load
-    //Project Video
-    $("#projects-video").each(function(){
-        if (projectPlayed == 0) {
-            if ($("#projects-video").is(":in-viewport")) {
-            $("#projects-video")[0].play();
-            projectPlayed = 1;
-            }
-        }
+    //Set interactivity for projects
+    LottieInteractivity.create({
+        player: "#projects-animation",
+        mode: "scroll",
+        actions: [{
+            visibility: [0.25, 1.0],
+            type: "play"
+        }],
+
     });
+    
 //About Video
     $("#about-video").each(function(){
         if (aboutPlayed == 0) {
@@ -67,18 +68,6 @@ $(document).ready(function(){
     });
 });
 
-//Check if videos become visible on user scroll
-//Projects Video
-$(window).scroll(function() {
-    $("#projects-video").each(function(){
-        if (projectPlayed == 0) {
-            if ($("#projects-video").is(":in-viewport")) {
-            $("#projects-video")[0].play();
-            projectPlayed = 1;
-            }
-        }
-    });
-});
 //About Video
 $(window).scroll(function() {
 $				("#about-video").each(function(){
